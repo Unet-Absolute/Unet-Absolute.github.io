6f11c901e4e562ce8bea1073b242cba2aa36114c
TommyShaw <Thomas_n-shaw@hotmail.com>
2019-10-14 17:57:05 -0400

    trying to fix

--- src/NavBar.js
+++ src/NavBar.js
@@ -13,7 +13,7 @@ import InstaG from './ig-logo-email.png';
 
 function NavBar(){
     return(
-        <Router basename="DDblog">
+        <Router>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <span className="navbar-brand" >DataDrone</span>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
