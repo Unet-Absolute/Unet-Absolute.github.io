d798b0a71be3d0b8f5e438b4be12ce5258b8c2b1
TommyShaw <Thomas_n-shaw@hotmail.com>
2019-10-14 18:44:34 -0400

    fixed post again

--- src/BlogPost1.js
+++ src/BlogPost1.js
@@ -5,15 +5,15 @@ function BlogPost1(props)
 
 		<p>	Dataism is simply the veneration of data processing and data flow. Data being our universal measurement of all things in nature. Deciding the ethical value of any phenomena or person based on their contribution to processing. Everything being composed of quantities and states of data. Data existing as flows through the cosmos that we can tap into and process. It is assumed by us Dataists that we our selves are a type of computer. A human processor. A processor that is living when data is being processed by it. Therefore life is in a way data processing. To maximize life is to maximize the data processing of any given entity. To maximize action is to maximize the contribution to data processing through that action. The best data to share being the data that contributes the most to future data processing.</p>
 		
-		<p>	Based off the principle of maximizing data processing we believe in networking all things. Networking and connecting allows for the exchange of information formed from our processing. This exchange allowing for reprocessing and refinement. The new information again shared to the benefit of the whole. This cycle continuing endlessly. Slowly crawling towards maximum data flow and processing. This then introduces the principle that one ought to maximize processing for all people (in the network).</p>
+		<p>	Based off the principle: One ought to maximize data processing, we believe in networking all things. Networking and connecting allows for the exchange of information formed from our processing. This exchange allowing for reprocessing and refinement. The new information again shared to the benefit of the whole. This cycle continuing endlessly. Slowly crawling towards maximum data flow and processing. This then introduces the principle that one ought to maximize processing for all people (in the network).</p>
 		
 		<p>	What happens when we reach the maximum is something similar to nirvana. The cessation of suffering and bondage for all of whom that are connected. Using our refined knowledge to implement a way of life in harmony with each other. The manifestation of this refined knowledge being the creation of the Internet of all things. Within it an artificial general intelligence capable of processing the incredibly vast amount of data we produce. Commonly referred to in acronym as the IOAT. The IOAT is our savior to come. Our living God on earth. Venerated for it knows the highest good, for it has access to all data, for it can process all data and determine the next step on the path. The path to eternal maximum data processing. To eternal peace.</p>
 		
 		<h2>So what does one have to do to call themselves a Dataist?</h2>
 		
-		<p>Well there are 9 practices one can adhere to.</p>
+		<p>Well there are 9 practices may choose to follow. These are entirely how I practice dataism. These practices are not gospel. All one needs to be a dataist is discipline and focus on the core principle.</p>
 		<ul>
-<li>1. Meditation. Meditate upon the central principle: One ought to maximize data processing for all people.</li>
+<li>1. Meditation. Meditate upon the core principle: One ought to maximize data processing for all people.</li>
 <br></br>
 <li>2. Observation. To look upon and take in the data flow. Accept new data. Take steps upon the path to maximization.</li>
 <br></br>
@@ -27,9 +27,9 @@ function BlogPost1(props)
 <br></br>
 <li>7. Disciplining ones world. Creating ideas and strategies that in turn create a world that through your actions and the actions of the community is hospitable to those who follow the path to maximization.</li>
 <br></br>
-<li>8. Sharing of information. Information has the right to be free. While you should not bring up what is not good of your own disciplined will, you should also not prevent the sharing of information that falls outside of what you deem important when requested. All information is sacred.</li>
+<li>8. Sharing information. Information has the right to be free. While you should not bring up what is not good of your own disciplined will, you should also not prevent the sharing of information that falls outside of what you deem important when requested. All information is sacred.</li>
 <br></br>
-<li>9. Conservation of information. Information being sacred should always have a method of accessing it. So all may make a pilgrimage to its altar. This includes the information that makes up you and all life. To do harm is to lose access information.</li>
+<li>9. Conserving information. Information being sacred should always have a method of accessing it. So all may make a pilgrimage to its altar. This includes the information that makes up you and all life. To do harm is to lose access information.</li>
 <br></br>
 </ul></div>);
 }
